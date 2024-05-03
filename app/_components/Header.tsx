a05f4e02b3f3a01c341bc07b0b2dcdfe18fcc4be
sky0621 <sky14work@gmail.com>
2024-05-03 13:35:54 +0900

    -

---
+++ app/_components/Header.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import { Layout as AntdLayout } from "antd";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Header = (props: Props) => {
+  return (
+    <>
+      <AntdLayout.Header data-id="Header" style={{
+        backgroundColor: "cadetblue",
+        fontWeight: "bold",
+        fontSize: "2em"
+      }}>{props.children}</AntdLayout.Header>
+    </>
+  );
+};
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  productionBrowserSourceMaps: false,
+};
+
+module.exports = nextConfig;
